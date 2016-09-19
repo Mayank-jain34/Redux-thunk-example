d33aecf3a7efb3e972e1cf409ef7f8e65592ccad
Mayank Jain <mayank_jain1@persistent.co.in>
2016-09-19 19:24:47 +0530

    First Commit

---
+++ store/configureStore.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+import rootReducer from '../reducers/reducers';
+
+const loggerMiddleWare = createLogger();
+
+export default function configureStore(preloadedState) {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(
+      thunk,
+    )
+  );
+}
